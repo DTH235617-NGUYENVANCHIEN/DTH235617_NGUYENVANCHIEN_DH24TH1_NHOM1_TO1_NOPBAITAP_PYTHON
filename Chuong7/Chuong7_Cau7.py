22cb0738498f4f5626f051774c127541c62a94fc
Nguyễn Văn Chiến <chien_dth235617@student.agu.edu.vn>
2025-11-16 21:37:28 +0700

    kết thúc bài tập + bài tập SGK

--- Chuong7/Chuong7_Cau7.py
+++ Chuong7/Chuong7_Cau7.py
@@ -1,68 +1,33 @@
 import xlsxwriter
-# Tạo một file excel cùng 1 sheet
+
 workbook = xlsxwriter.Workbook('demo.xlsx')
 worksheet = workbook.add_worksheet()
-# thiết lập các cột cho file
+
 worksheet.set_column('A:A', 5)
 worksheet.set_column('B:B', 15)
 worksheet.set_column('C:C', 20)
 worksheet.set_column('D:D', 15)
 worksheet.set_column('E:E', 15)
-# định dạng tiêu đề cột in đậm
-bold = workbook.add_format({'bold': True})
-# thêm dòng tiêu đề và định dạng in đậm
 
-worksheet.write('A1', 'STT',bold)
-worksheet.write('B1', 'MÃ SẢN PHẨM',bold)
-worksheet.write('C1', 'TÊN SẢN PHẨM',bold)
-worksheet.write('D1', 'SỐ LƯỢNG',bold)
-worksheet.write('E1', 'ĐƠN GIÁ',bold)
-#thêm một dòng dữ liệu
-worksheet.write('A2',1)
-worksheet.write('B2','SP1')
+bold = workbook.add_format({'bold': True})
+
+worksheet.write('A1', 'STT', bold)
+worksheet.write('B1', 'MÃ SẢN PHẨM', bold)
+worksheet.write('C1', 'TÊN SẢN PHẨM', bold)
+worksheet.write('D1', 'SỐ LƯỢNG', bold)
+worksheet.write('E1', 'ĐƠN GIÁ', bold)
+
+worksheet.write('A2', '1')
+worksheet.write('B2', 'SP001')
 worksheet.write('C2', 'Coca')
-worksheet.write('D2', '15')
+worksheet.write('D2', '10')
 worksheet.write('E2', '15000')
-#thêm một dòng dữ liệu
-worksheet.write('A3',2)
-worksheet.write('B3','SP2')
+
+worksheet.write('A3', '2')
+worksheet.write('B3', 'SP002')
 worksheet.write('C3', 'Pepsi')
 worksheet.write('D3', '20')
-worksheet.write('E3', '18000')
-#Chèn Logo vào
-worksheet.insert_image('B5', 'logo_UEL.png')
-workbook.close()
-import xlsxwriter
-# Tạo một file excel cùng 1 sheet
-workbook = xlsxwriter.Workbook('demo.xlsx')
-worksheet = workbook.add_worksheet()
-# thiết lập các cột cho file
-worksheet.set_column('A:A', 5)
-worksheet.set_column('B:B', 15)
-worksheet.set_column('C:C', 20)
-worksheet.set_column('D:D', 15)
-worksheet.set_column('E:E', 15)
-# định dạng tiêu đề cột in đậm
-bold = workbook.add_format({'bold': True})
-# thêm dòng tiêu đề và định dạng in đậm
+worksheet.write('E3', '13000')
 
-worksheet.write('A1', 'STT',bold)
-worksheet.write('B1', 'MÃ SẢN PHẨM',bold)
-worksheet.write('C1', 'TÊN SẢN PHẨM',bold)
-worksheet.write('D1', 'SỐ LƯỢNG',bold)
-worksheet.write('E1', 'ĐƠN GIÁ',bold)
-#thêm một dòng dữ liệu
-worksheet.write('A2',1)
-worksheet.write('B2','SP1')
-worksheet.write('C2', 'Coca')
-worksheet.write('D2', '15')
-worksheet.write('E2', '15000')
-#thêm một dòng dữ liệu
-worksheet.write('A3',2)
-worksheet.write('B3','SP2')
-worksheet.write('C3', 'Pepsi')
-worksheet.write('D3', '20')
-worksheet.write('E3', '18000')
-#Chèn Logo vào
-worksheet.insert_image('B5', 'logo_UEL.png')
+worksheet.insert_image('B5', 'logo_UEI.png')
 workbook.close()
